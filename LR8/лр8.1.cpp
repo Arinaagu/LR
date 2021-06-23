f0bdc46120c5e716133c52da6e48cbd9289d51f4
Arinaagu <arinag1968@gmail.com>
2021-06-23 23:53:30 +0400

    Create лр8.1.cpp

---
+++ LR8/лр8.1.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n; int k; int i = 10000;
+	cout << "Введите пятизначное число" << endl;
+	cin >> n;
+	while (n > 0)
+	{
+		k = n / i; //вычленение цифры;
+		cout << k << endl;
+		n = n - k * i; // уменьшение числа на старший разряд;
+		i = i / 10;
+	}
+
+	system("pause");
+	return 0;
+}
+
