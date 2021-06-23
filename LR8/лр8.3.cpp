7f8d45f3c3dd79bd81eb8913d80e60c7d2cb40ea
Arinaagu <arinag1968@gmail.com>
2021-06-24 00:01:22 +0400

    Create лр8.3.cpp

---
+++ LR8/лр8.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+const int M = 5;
+int avg(int a[M])
+{
+	int sum = 0, avg = 0;
+	for (int i = 0; i < M; i++)
+	{		//ф-я поиска среднего арифметического в массиве                     
+		sum = sum + a[i];              
+
+	}
+	avg = sum / M;
+	return avg;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int b[M] = { 28,17,93,88,7 };
+	cout << "Среднее арифметическое массива=" << avg(b);
+	system("pause");
+	return 0;
+}
