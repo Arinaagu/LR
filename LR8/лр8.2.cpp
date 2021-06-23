45cd5267830a58456edb0cde56006f66b0969177
Arinaagu <arinag1968@gmail.com>
2021-06-23 23:57:17 +0400

    Create лр8.2.cpp

---
+++ LR8/лр8.2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char a;
+	cout << "Введите строчную букву:" << endl;
+	cin >> a;
+	a = toupper(a); //функция перевода регистра;
+	cout << "Вывод заглавной буквы:" << endl << a;
+	system("pause");
+	return 0;
+}
